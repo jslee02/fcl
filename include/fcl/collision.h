7149f6bb422aa290bc1b1b91ad127e6f788022f3
Jeongseok Lee <jslee02@users.noreply.github.com>
2016-06-16 12:04:06 -0400

    Update collision.h

--- include/fcl/collision.h
+++ include/fcl/collision.h
@@ -58,7 +58,7 @@ std::size_t collide(const CollisionObject* o1, const CollisionObject* o2,
 std::size_t collide(const CollisionGeometry* o1, const Transform3f& tf1,
                     const CollisionGeometry* o2, const Transform3f& tf2,
                     const CollisionRequest& request,
-                    CollisionResult& result);
+                    CollisionResult& result);a
 }
 
 #endif
